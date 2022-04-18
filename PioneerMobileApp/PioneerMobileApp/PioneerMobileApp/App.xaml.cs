d380229ba7b6d03157e2950d226f1e9d84d893f9
CristinaDami <cdamian86@yahoo.com>
2022-04-18 22:57:45 +0100

    Login page created

---
+++ PioneerMobileApp/PioneerMobileApp/PioneerMobileApp/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using PioneerMobileApp.Services;
+using PioneerMobileApp.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PioneerMobileApp
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+
+            // MainPage = new AppShell();
+            MainPage = new NavigationPage(new LoginUI());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ PioneerMobileApp/PioneerMobileApp/PioneerMobileApp/LoginUI.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PioneerMobileApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginUI : ContentPage
+    {
+        public LoginUI()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtUsername.Text) && string.IsNullOrEmpty(txtPassword.Text))
+            {
+                DisplayAlert("Ops...", "Username and Password cannot be empty!", "Ok");
+            }
+            else if (txtUsername.Text == "Admino" && txtPassword.Text == "Admino")
+            {
+                Navigation.PushAsync(new HomePage());
+            }
+            else if (txtUsername.Text == "Adminf" && txtPassword.Text == "Adminf")
+            {
+                Navigation.PushAsync(new HomePage());
+            }
+            else if (txtUsername.Text == "Ope1" && txtPassword.Text == "Ope1")
+            {
+                Navigation.PushAsync(new HomePage());
+            }
+            else
+            {
+                DisplayAlert("Ops...", "Username or Password not correct!", "Ok");
+            }
+        }
+
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new RegisterPage());
+        }
+    }
+}
